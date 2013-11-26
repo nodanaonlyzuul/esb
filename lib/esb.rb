b6203679586d87d3718ec4d0f21959b286a293c2
Stephen Schor <stephen@eastmedia.com>
2013-11-26 17:25:40 -0500

    outputting lighting

--- lib/esb.rb
+++ lib/esb.rb
@@ -3,7 +3,9 @@ require 'open-uri'
 
 class Esb
   def self.get_color
-    doc = Nokogiri::HTML(open("www.esbnyc.com/current_events_tower_lights.asp"))
-    require 'pry';binding.pry;
+    doc = Nokogiri::HTML(open("http://www.esbnyc.com/current_events_tower_lights.asp"))
+    puts " "
+    puts doc.css("#lighting_schedule table tr.current").text.gsub("\t", "")
+    puts " "
   end
 end
